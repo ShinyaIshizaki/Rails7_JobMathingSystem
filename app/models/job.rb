26758c1c62b13251fc0ec50523953b1d556cfbd0
ShinyaIshizaki <shinya.ishizaki.0311@gmail.com>
2023-06-10 17:49:20 +0900

    remaked selectbox of telework

--- app/models/job.rb
+++ app/models/job.rb
@@ -11,4 +11,10 @@ class Job < ApplicationRecord
         福岡県:40,佐賀県:41,長崎県:42,熊本県:43,大分県:44,宮崎県:45,鹿児島県:46,
         沖縄県:47
       }
+
+    enum telework_flg:{
+        不可:0,
+        一部可:1,
+        可:2
+    }
 end

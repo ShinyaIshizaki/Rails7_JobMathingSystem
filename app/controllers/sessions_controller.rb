08edddbfd3be07f3bb5b680d247a67d0b822bccf
ShinyaIshizaki <shinya.ishizaki.0311@gmail.com>
2023-06-14 21:01:31 +0900

    maked login logout

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -10,6 +10,7 @@ class SessionsController < ApplicationController
     if user && user.authenticate(params[:session][:password])
       # ユーザーログイン後にユーザー情報のページにリダイレクトする
       log_in user
+      params[:session][:remember_me] == '1' ? remember(user) : forget(user)
       redirect_to user
     else
       # エラーメッセージを作成する
@@ -19,5 +20,8 @@ class SessionsController < ApplicationController
   end
 
   def destroy
+    # ログイン中の場合のみログアウトする
+    log_out if logged_in?
+    redirect_to root_url
   end
 end

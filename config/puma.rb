aa92ee87f0bfee0ef22c0589fb8e661718dd3453
ShinyaIshizaki <shinya.ishizaki.0311@gmail.com>
2023-06-21 17:01:19 +0900

    commentouted port description

--- config/puma.rb
+++ config/puma.rb
@@ -15,7 +15,10 @@ worker_timeout 3600 if ENV.fetch("RAILS_ENV", "development") == "development"
 
 # Specifies the `port` that Puma will listen on to receive requests; default is 3000.
 #
-port ENV.fetch("PORT") { 3000 }
+# port ENV.fetch("PORT") { 3000 }
+
+#ソケットファイルの作成を指定する
+bind "unix://#{Rails.root}/tmp/sockets/puma.sock"
 
 # Specifies the `environment` that Puma will run in.
 #
@@ -41,6 +44,3 @@ pidfile ENV.fetch("PIDFILE") { "tmp/pids/server.pid" }
 
 # Allow puma to be restarted by `bin/rails restart` command.
 plugin :tmp_restart
-
-# Nginxとソケット通信をする際に必要になるファイル
-bind "unix://#{Rails.root}/tmp/sockets/puma.sock"
